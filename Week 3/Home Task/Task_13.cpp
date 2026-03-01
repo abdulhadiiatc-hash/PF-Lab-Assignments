68f1479e30d84ce65828115055e1abc8c145d494
abdulhadiiatc.hash <abdulhadiiatc@gmail.com>
2026-03-01 21:32:12 +0500

    Add task13.cpp

---
+++ Week 3/Home Task/Task_13.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int num,sum;
+cout<<"Enter 5 Integers:\n";
+cin>>num;
+sum = sum + num;
+cin>>num;
+sum = sum + num;
+cin>>num;
+sum = sum + num;
+cin>>num;
+sum = sum + num;
+cin>>num;
+sum = sum + num;
+cout<<"Sum= "<<sum;
+}
